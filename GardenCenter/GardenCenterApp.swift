fd9ce255fe5a8de7eb71c6e0cea547e5f9c2cbbd
wileyhodges <wileyhodges@users.noreply.github.com>
2021-08-26 15:26:12 -0700

    Initial Commit

---
+++ GardenCenter/GardenCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenCenterApp.swift
+//  GardenCenter
+//
+//  Created by Wiley Hodges on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct GardenCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
